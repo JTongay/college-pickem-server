950700f39a14f80cd5c7d3533ce1f432b8d72243
Joseph Tongay <dirk.tongay@gmail.com>
2017-09-22 19:24:53 -0500

    working with a linter is weird lol

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 
-const something = (bowdy)=>{
-  console.log(booyah);
-}
+const app = express();
+const port = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV || 'development';
+const config = require('./knexfile')[environment];
+const knex = require('knex')(config);
+require('dotenv').config();
+
+// Middleware
+const bodyParser = require('body-parser');
+
+// Use Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.user((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.listen(port, () => {
+  // eslint-disable no-alert, no-console
+  console.log('Hello from port ', port);
+  console.log('Starting in mode ', environment);
+  // eslint-enable no-alert
+});
+
+module.exports = app;

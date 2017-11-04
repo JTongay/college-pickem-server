a8f33de6f87b3130bab97aa9755be4e30cfa1958
Joseph Tongay <dirk.tongay@gmail.com>
2017-11-03 20:08:32 -0500

    ran some migrations and did more with testing

--- db/migrations/20171103144044_create_season_table.js
+++ db/migrations/20171103144044_create_season_table.js
@@ -1,10 +1,12 @@
-exports.up = knex => knex.createTableIfNotExists('seasons', (table) => {
-  table.increments();
-  table.string('league');
-  table.date('start_date');
-  table.date('end_date');
-  table.boolean('active_season');
-  table.timestamps(true, true);
-});
+exports.up = (knex, Promise) => {
+  return knex.schema.createTableIfNotExists('seasons', (table) => {
+    table.increments();
+    table.string('league');
+    table.date('start_date');
+    table.date('end_date');
+    table.boolean('active_season').defaultTo(true);
+    table.timestamps(true, true);
+  });
+};
 
 exports.down = knex => knex.dropTableIfExists('seasons');
--- test/seasons.spec.js
+++ test/seasons.spec.js
@@ -10,11 +10,23 @@ const moment = require('moment');
 
 before((done) => {
   knex('seasons').insert({
-
+    league: 'NCAA',
+    start_date: moment.year('2017').month('09').date('05'),
+    end_date: moment.year('2018').month('01').date('10'),
+    active_season: true
+  }).then(() => {
+    done();
   });
 });
 
-describe('Seasons', () => {
+after((done) => {
+  console.log('truncating seasons');
+  knex.raw('truncate table seasons RESTART IDENTITY;').then(() => {
+    done();
+  });
+});
+
+describe.only('Seasons', () => {
   describe('/ GET', () => {
     it('Returns a list of seasons', (done) => {
       request.get('/api/season/')

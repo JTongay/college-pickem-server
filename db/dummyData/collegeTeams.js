57be41d246ae0ecb298c504a400c7559163b2326
Joseph Tongay <dirk.tongay@gmail.com>
2017-11-22 19:44:57 -0600

    boom

---
+++ db/dummyData/collegeTeams.js
@@ -0,0 +1,67 @@
+const collegeTeams = [
+  {
+    team_name: 'Longhorns',
+    abbr_name: 'TEX',
+    location: 'Texas',
+    league: 'NCAA',
+    record: '2-0',
+    rank: 3
+  },
+  {
+    team_name: 'Buckeyes',
+    abbr_name: 'OSU',
+    location: 'Ohio State',
+    league: 'NCAA',
+    record: '2-1',
+    rank: 24
+  },
+  {
+    team_name: 'Crimson Tide',
+    abbr_name: 'ALA',
+    location: 'Alabama',
+    league: 'NCAA',
+    record: '3-0',
+    rank: 1
+  },
+  {
+    team_name: 'Sooners',
+    abbr_name: 'OKLA',
+    location: 'Oklahoma',
+    league: 'NCAA',
+    record: '0-2',
+    rank: null
+  },
+  {
+    team_name: 'Broncos',
+    abbr_name: 'BSU',
+    location: 'Texas',
+    league: 'NCAA',
+    record: '2-0',
+    rank: 3
+  },
+  {
+    team_name: 'Badgers',
+    abbr_name: 'WIS',
+    location: 'Wisconsin',
+    league: 'NCAA',
+    record: '1-3',
+    rank: null
+  },
+  {
+    team_name: 'Horned Frogs',
+    abbr_name: 'TCU',
+    location: 'Texas Commonwealth',
+    league: 'NCAA',
+    record: '2-1',
+    rank: 14
+  },
+  {
+    team_name: 'Blue Devils',
+    abbr_name: 'DUKE',
+    location: 'Duke',
+    league: 'NCAA',
+    record: '0-3'
+  }
+];
+
+module.exports = collegeTeams;
--- db/seeds/create_test_data.js
+++ db/seeds/create_test_data.js
@@ -1,16 +1,21 @@
 const seasons = require('../dummyData/seasons');
 const nflTeams = require('../dummyData/nflTeams');
+const collegeTeams = require('../dummyData/collegeTeams');
 
 exports.seed = (knex, Promise) =>
   // Deletes ALL existing entries
-  knex.raw('TRUNCATE TABLE teams RESTART IDENTITY CASCADE')
+  knex.raw('TRUNCATE TABLE seasons RESTART IDENTITY CASCADE')
     .then(() =>
-      // Inserts seed entries
-      Promise.all([
-        knex('seasons').insert(seasons).then(res => res),
-        knex('teams').insert(nflTeams).then(res => res)
-      ]).then(() => {
-        console.log('success');
-      })
+      knex.raw('TRUNCATE TABLE teams RESTART IDENTITY CASCADE')
+        .then(() =>
+        // Inserts seed entries
+          Promise.all([
+            knex('seasons').insert(seasons).then(res => res),
+            knex('teams').insert(nflTeams).then(res => res),
+            knex('teams').insert(collegeTeams).then(res => res)
+          ]).then(() => {
+            console.log('success');
+          })
+        )
     )
 ;

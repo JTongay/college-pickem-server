a16cd1e44682128ac6c94e2d0214a2749a47b650
Joseph Tongay <dirk.tongay@gmail.com>
2017-11-25 19:15:11 -0600

    this feels performantly fine, but i want to get rid of the excess properties

--- routes/matchups.js
+++ routes/matchups.js
@@ -9,12 +9,23 @@ const knex = require('../db/conf');
 
 router.get('/', (req, res) => {
   const seasonId = req.params.season_id;
-  knex('matchups').where('season_id', seasonId).innerJoin('seasons', 'matchups.season_id', 'seasons.id').then((match) => {
-    console.log(match);
-    res.status(200).json({
-      match
+  knex('matchups').where('season_id', seasonId).innerJoin('seasons', 'matchups.season_id', 'seasons.id')
+    .then((match) => {
+      knex('teams').innerJoin('matchups', 'teams.id', 'matchups.home_team_id')
+        .then((homeTeam) => {
+          knex('teams').innerJoin('matchups', 'teams.id', 'matchups.away_team_id')
+            .then((awayTeam) => {
+              const showMatches = match.map((game, index) => {
+                game.away_team = awayTeam[index]; // eslint-disable-line no-param-reassign
+                game.home_team = homeTeam[index]; // eslint-disable-line no-param-reassign
+                return game;
+              });
+              res.status(200).json({
+                showMatches
+              });
+            });
+        });
     });
-  });
 });
 
 // router.get('/:id', (req, res) => {
